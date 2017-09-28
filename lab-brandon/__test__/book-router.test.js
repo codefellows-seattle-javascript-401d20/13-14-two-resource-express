2515cde45311e2ec1f73a40307b3afc7b3bfc950
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-09-28 09:02:41 -0700

    messed with mongodb

--- lab-brandon/__test__/book-router.test.js
+++ lab-brandon/__test__/book-router.test.js
@@ -1,7 +1,7 @@
-Book'use strict';
+'use strict';
 
 // mock the env
-process.env.PORT = 7000;
+process.env.PORT = 4000;
 process.env.CORS_ORIGIN = 'http://localhost:8080';
 process.env.MONGODB_URI = 'mongodb://localhost/testing';
 
